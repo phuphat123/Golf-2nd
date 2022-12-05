a3476fdd43d899e4450a5d0300e583f11fd44f75
deepenthapa <deepenthapa@protonmail.com>
2022-12-05 04:22:32 +0000

    tp2 and map changes

--- Assets/TeleporterTwo.cs
+++ Assets/TeleporterTwo.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleporterOne : MonoBehaviour
+public class TeleporterTwo : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private Transform TpPoint1;
+    [SerializeField] private Transform TpPoint2;
 
 
     private void OnTriggerEnter(Collider other)
     {
+      
         if (other.gameObject.CompareTag("Player"))
         {
-
-            player.transform.position = TpPoint1.transform.position;
+            player.transform.position = TpPoint2.transform.position;
         }
-
     }
 }
