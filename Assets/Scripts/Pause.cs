ad0bdabf96c7a99491fac28ad338f6084ddf22d3
deepenthapa <deepenthapa@protonmail.com>
2022-12-07 11:28:41 +0000

    settingsMenu

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -50,6 +50,7 @@ public class Pause : MonoBehaviour
         
 
     }
+   
 
     // Exits game 
     public void QuitGame()
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    public Dropdown resDropdown;
+    Resolution[] resolutions; // creating an array for resolutions
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions(); // clears resolutions in the dropdown before adding users res
+
+        List<string> options = new List<string>(); //creating list for the all the resolutions
+        int currentResolutionIndex = 0;
+        // loops through all resolutions in the array
+        for (int i = 0;i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height; // each element creating string for width and length of resolution 
+            options.Add(option); // adds res options to list 
+            // checks for resolution of the users screen by comparing width and height
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)   
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+        resDropdown.AddOptions(options);// add res to dropdown
+        resDropdown.value = currentResolutionIndex;// sets the value of users screen 
+        resDropdown.RefreshShownValue();
+
+
+
+
+    }
+    // checks if users screen is in fullscreen
+    void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    // Mainmenu volume mixer
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        mainMixer.SetFloat("volume1", volume);
+    }
+    // graphic settings
+    public void SetQuality (int graphicsIndex) 
+    {
+        QualitySettings.SetQualityLevel(graphicsIndex);
+    }
+}
